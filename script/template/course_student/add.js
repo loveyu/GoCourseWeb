0a489e44274e48fd3ab1c19106327e623a03312c
loveyu <admin@loveyu.info>
2015-05-07 13:50:40 +0800

    修正学生选课的事件逻辑

--- script/template/course_student/add.js
+++ script/template/course_student/add.js
@@ -1,8 +1,13 @@
 _methods_ = {
 	onAdd: function (id) {
 		var obj = this;
+		obj.error = "";
 		FUNC.ajax(CONFIG.api.course_table.student_select_add, "post", {id: obj.list[id].course.courseTableID}, function (result) {
-			console.log(result);
+			if (result.status) {
+				obj.list[id].selected = 1;
+			} else {
+				obj.error = result.msg;
+			}
 		});
 	}
 };//_methods_
