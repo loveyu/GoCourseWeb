2314b03f5f81c49d6177bb6a84dd2e55dc376885
loveyu <admin@loveyu.info>
2015-05-28 22:44:46 +0800

    添加状态码生成工具

---
+++ gen_status_index.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * User: loveyu
+ * Date: 2015/5/28
+ * Time: 22:19
+ */
+$package = isset($argv[2]) ? $argv[2] : "G:\\J2EE\\GoCourseServer\\src\\com\\katoa\\gocourse\\model\\action";
+
+$list = glob($package . "/*/*.java");
+$data = [];
+foreach ($list as $v) {
+	$content = file_get_contents($v);
+	if (preg_match("/\\* #([\\s\\S]+?)\n/", $content, $m) !== 1) {
+		continue;
+	}
+	if (preg_match("/super\\(([1-9]{1}[0-9]{2}?)\\);/", $content, $m2) !== 1) {
+		continue;
+	}
+
+	if (preg_match("/public class ([a-zA-Z_]+?) extends BaseAction/", $content, $m3) !== 1) {
+		continue;
+	}
+	$data[$m2[1]] = ["title" => trim($m[1]), "class" => trim($m3[1])];
+}
+ksort($data);
+$i = 1;
+foreach ($data as $key => $value) {
+	echo $i++,". ",$value['title']," `{$value['class']}` `{$key}`\r\n";
+}
